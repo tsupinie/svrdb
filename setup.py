bed092629eeb339f467be2ecce8a50eb09bfd9ca
tsupinie <tsupinie@gmail.com>
2018-12-10 00:07:52 -0600

    Added setup.py script

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup, find_packages
+import svrdb._svrdb_version as version
+
+setup(
+    name = 'svrdb',
+    version = version.get_version(),
+    author = 'Tim Supinie',
+    author_email = 'tsupinie@gmail.com',
+    description = 'SPC severe weather database tools',
+    license = 'GPLv3',
+    keywords = 'meteorology spc reports',
+    url = 'https://github.com/tsupinie/svrdb',
+    packages = ['svrdb'],
+    package_data = {'svrdb':['svrdb/data/*']},
+    include_package_data = True,
+)
---
+++ svrdb/_svrdb_version.py
@@ -0,0 +1,5 @@
+
+_vers = '0.1'
+
+def get_version():
+    return _vers
